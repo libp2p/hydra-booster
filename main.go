d2e7316f0b1a06efba408c4bf69946199fd222ad
whyrusleeping <why@ipfs.io>
2019-06-26 14:32:29 +0200

    add pprof metrics

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"expvar"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
-	_ "net/http/pprof"
+	"net/http/pprof"
 	"os"
 	"runtime"
 	"time"
@@ -319,15 +320,15 @@ func setupMetrics(addr string) error {
 		mux := http.NewServeMux()
 		zpages.Handle(mux, "/debug")
 		mux.Handle("/metrics", pe)
-		// mux.Handle("/debug/vars", expvar.Handler())
-		// mux.HandleFunc("/debug/pprof", pprof.Index)
+		mux.Handle("/debug/vars", expvar.Handler())
+		mux.HandleFunc("/debug/pprof", pprof.Index)
 		// mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-		// mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+		mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
 		// mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-		// mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+		mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 		// mux.Handle("/debug/pprof/block", pprof.Handler("block"))
-		// mux.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
-		// mux.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+		mux.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
+		mux.Handle("/debug/pprof/heap", pprof.Handler("heap"))
 		// mux.Handle("/debug/pprof/mutex", pprof.Handler("mutex"))
 		// mux.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
 		if err := http.ListenAndServe(promAddr, mux); err != nil {
