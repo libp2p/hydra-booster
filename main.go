2f7f69bd30a3fb32fa79e7cc264f23c6738ce59e
Steven Allen <steven@stebalien.com>
2019-10-23 10:21:21 -0700

    fix: use default bootstrap list

--- main.go
+++ main.go
@@ -37,7 +37,6 @@ import (
 	pstore "github.com/libp2p/go-libp2p-peerstore"
 	record "github.com/libp2p/go-libp2p-record"
 	id "github.com/libp2p/go-libp2p/p2p/protocol/identify"
-	ma "github.com/multiformats/go-multiaddr"
 	prom "github.com/prometheus/client_golang/prometheus"
 	"go.opencensus.io/stats/view"
 	"go.opencensus.io/zpages"
@@ -85,23 +84,9 @@ func waitForNotifications(r io.Reader, provs chan *provInfo, mesout chan string)
 	}
 }
 
-var bootstrappers = []string{
-	"/ip4/104.131.131.82/tcp/4001/ipfs/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",  // mars.i.ipfs.io
-	"/ip4/104.236.179.241/tcp/4001/ipfs/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM", // pluto.i.ipfs.io
-	"/ip4/128.199.219.111/tcp/4001/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu", // saturn.i.ipfs.io
-	"/ip4/104.236.76.40/tcp/4001/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64",   // venus.i.ipfs.io
-	"/ip4/178.62.158.247/tcp/4001/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd",  // earth.i.ipfs.io
-}
-
 func bootstrapper() pstore.PeerInfo {
-	bsa := bootstrappers[rand.Intn(len(bootstrappers))]
-
-	a, err := ma.NewMultiaddr(bsa)
-	if err != nil {
-		panic(err)
-	}
-
-	ai, err := pstore.InfoFromP2pAddr(a)
+	addr := dht.DefaultBootstrapPeers[rand.Intn(len(dht.DefaultBootstrapPeers))]
+	ai, err := pstore.InfoFromP2pAddr(addr)
 	if err != nil {
 		panic(err)
 	}
