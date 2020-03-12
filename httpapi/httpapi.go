936dabd36a805eb7580c3309de86d9692bd77f73
Alan Shaw <alan@tableflip.io>
2020-03-12 10:22:18 +0000

    refactor: remove runner and add Hydra struct

--- httpapi/httpapi.go
+++ httpapi/httpapi.go
@@ -8,44 +8,44 @@ import (
 
 	dsq "github.com/ipfs/go-datastore/query"
 	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/hydra-booster/sybil"
+	"github.com/libp2p/hydra-booster/hydra"
 )
 
 // ListenAndServe instructs a Hydra HTTP API server to listen and serve on the passed address
-func ListenAndServe(sybils []*sybil.Sybil, addr string) error {
+func ListenAndServe(hy *hydra.Hydra, addr string) error {
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
 		return err
 	}
-	return http.Serve(listener, NewServeMux(sybils))
+	return http.Serve(listener, NewServeMux(hy))
 }
 
 // NewServeMux creates a new Hydra Booster HTTP API ServeMux
-func NewServeMux(sybils []*sybil.Sybil) *http.ServeMux {
+func NewServeMux(hy *hydra.Hydra) *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/sybils", sybilsHandler(sybils))
-	mux.HandleFunc("/records/fetch", recordFetchHandler(sybils))
-	mux.HandleFunc("/records/list", recordListHandler(sybils))
+	mux.HandleFunc("/sybils", sybilsHandler(hy))
+	mux.HandleFunc("/records/fetch", recordFetchHandler(hy))
+	mux.HandleFunc("/records/list", recordListHandler(hy))
 	return mux
 }
 
 // "/sybils" Get the peers created by hydra booster (ndjson)
-func sybilsHandler(nodes []*sybil.Sybil) func(w http.ResponseWriter, r *http.Request) {
+func sybilsHandler(hy *hydra.Hydra) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		enc := json.NewEncoder(w)
 
-		for _, n := range nodes {
+		for _, syb := range hy.Sybils {
 			enc.Encode(peer.AddrInfo{
-				ID:    n.Host.ID(),
-				Addrs: n.Host.Addrs(),
+				ID:    syb.Host.ID(),
+				Addrs: syb.Host.Addrs(),
 			})
 		}
 	}
 }
 
 // "/records/fetch" Receive a record and fetch it from the network, if available
-func recordFetchHandler(sybils []*sybil.Sybil) func(w http.ResponseWriter, r *http.Request) {
+func recordFetchHandler(hy *hydra.Hydra) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// TODO needs functionality to be implemented in libp2p
 		// See: https://discuss.libp2p.io/t/does-a-findproviders-replicate-the-provider-records-to-the-node-issuing-the-query/452
@@ -55,14 +55,14 @@ func recordFetchHandler(sybils []*sybil.Sybil) func(w http.ResponseWriter, r *ht
 }
 
 // "/records/list" Receive a record and fetch it from the network, if available
-func recordListHandler(sybils []*sybil.Sybil) func(w http.ResponseWriter, r *http.Request) {
+func recordListHandler(hy *hydra.Hydra) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// TODO Improve this handler once ProvideManager gets exposed
 		// https://discuss.libp2p.io/t/list-provider-records/450
 		// for now, enumerate the Provider Records in the datastore
 
 		providersKeyPrefix := "/providers/" // https://github.com/libp2p/go-libp2p-kad-dht/blob/master/providers/providers.go#L76
-		ds := sybils[0].Datastore
+		ds := hy.SharedDatastore
 		results, err := ds.Query(dsq.Query{
 			Prefix: providersKeyPrefix,
 		})
