c7360be729ea7cd892219465a313677c094b5670
Alan Shaw <alan@tableflip.io>
2020-04-16 11:18:06 +0100

    0.2.0

--- version/version.go
+++ version/version.go
@@ -1,6 +1,8 @@
 package version
 
 const (
-	Version   = "0.1.7"
+	// Version number of the Hydra Booster node, it should be kept in sync with the current release tag.
+	Version = "0.2.0"
+	// UserAgent is the string passed by the identify protocol to other nodes in the network.
 	UserAgent = "hydra-booster/" + Version
 )
