e4f3662b6591372e65f6fddd3aca36f953837d67
Alan Shaw <alan.shaw@protocol.ai>
2020-05-03 00:47:54 +0100

    chore: tweak conn config

--- datastore/postgres.go
+++ datastore/postgres.go
@@ -3,13 +3,18 @@ package datastore
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	sqlds "github.com/ipfs/go-ds-sql"
 	"github.com/ipfs/go-ds-sql/postgres"
 	_ "github.com/jackc/pgx/v4/stdlib" // postgres driver
 )
 
-const tableName = "records"
+const (
+	tableName       = "records"
+	maxIdleConns    = 25 // TODO tie to number of heads?
+	connMaxLifetime = time.Hour
+)
 
 // NewPostgreSQLDatastore creates a new sqlds.Datastore that talks to a PostgreSQL database
 func NewPostgreSQLDatastore(connstr string) (*sqlds.Datastore, error) {
@@ -17,6 +22,9 @@ func NewPostgreSQLDatastore(connstr string) (*sqlds.Datastore, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to open PostgreSQL database: %w", err)
 	}
+
+	db.SetMaxIdleConns(maxIdleConns)
+	db.SetConnMaxLifetime(connMaxLifetime)
 
 	_, err = db.Exec(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (key TEXT NOT NULL UNIQUE, data BYTEA NOT NULL)", tableName))
 	if err != nil {
