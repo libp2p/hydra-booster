2bb16a6a0dd3687fc91a00f000f21fbba5a4206f
Alan Shaw <alan.shaw@protocol.ai>
2020-05-14 22:39:19 +0100

    0.3.5

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 const (
 	// Version number of the Hydra Booster node, it should be kept in sync with the current release tag.
-	Version = "0.3.4"
+	Version = "0.3.5"
 	// UserAgent is the string passed by the identify protocol to other nodes in the network.
 	UserAgent = "hydra-booster/" + Version
 )
