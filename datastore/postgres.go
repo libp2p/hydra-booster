3268cb8d8937fc6186bf3d57f7a01200d68bb6f4
thattommyhall <thattommyhall@gmail.com>
2021-06-25 13:49:38 +0300

    log more as DB is instantiated
    It hangs rahter than errors, so this at least will tell us where we are in the process

--- datastore/postgres.go
+++ datastore/postgres.go
@@ -3,6 +3,7 @@ package datastore
 import (
 	"context"
 	"fmt"
+	"os"
 
 	pgds "github.com/alanshaw/ipfs-ds-postgres"
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -21,16 +22,19 @@ func NewPostgreSQLDatastore(ctx context.Context, connstr string, createDB bool) 
 		if err != nil {
 			return nil, err
 		}
+		fmt.Fprintf(os.Stderr, "Creating Table\n")
 		_, err = pool.Exec(ctx, fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (key TEXT NOT NULL UNIQUE, data BYTEA)", tableName))
 		if err != nil {
 			return nil, err
 		}
+		fmt.Fprintf(os.Stderr, "Creating Index\n")
 		_, err = pool.Exec(ctx, fmt.Sprintf("CREATE INDEX IF NOT EXISTS %s_key_text_pattern_ops_idx ON %s (key text_pattern_ops)", tableName, tableName))
 		if err != nil {
 			return nil, err
 		}
 		pool.Close()
 	}
+	fmt.Fprintf(os.Stderr, "Connecting to Database\n")
 	ds, err := pgds.NewDatastore(ctx, connstr, pgds.Table(tableName))
 	if err != nil {
 		return nil, err
