6ea23393b2ba3d797c82afb907f6b465018144bc
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-07-04 18:21:51 +0100

    Fix panic due to wrong addresses.

--- head/head.go
+++ head/head.go
@@ -57,22 +57,22 @@ func getAddrFilters() ([]*net.IPNet, error) {
 	// Source https://github.com/ipfs/go-ipfs-config/blob/0a474258a95d8d9436a49539ad9d7da357b015ab/profile.go#L27
 	// https://pkg.go.dev/github.com/ipfs/go-ipfs@v0.9.0/core/node/libp2p#AddrFilters
 	defaultAddrFilters := []string{
-		"/ip4/10.0.0.0/ipcidr/8",
-		"/ip4/100.64.0.0/ipcidr/10",
-		"/ip4/169.254.0.0/ipcidr/16",
-		"/ip4/172.16.0.0/ipcidr/12",
-		"/ip4/192.0.0.0/ipcidr/24",
-		"/ip4/192.0.2.0/ipcidr/24",
-		"/ip4/192.168.0.0/ipcidr/16",
-		"/ip4/198.18.0.0/ipcidr/15",
-		"/ip4/198.51.100.0/ipcidr/24",
-		"/ip4/203.0.113.0/ipcidr/24",
-		"/ip4/240.0.0.0/ipcidr/4",
-		"/ip6/100::/ipcidr/64",
-		"/ip6/2001:2::/ipcidr/48",
-		"/ip6/2001:db8::/ipcidr/32",
-		"/ip6/fc00::/ipcidr/7",
-		"/ip6/fe80::/ipcidr/10",
+		"10.0.0.0/8",
+		"100.64.0.0/10",
+		"169.254.0.0/16",
+		"172.16.0.0/12",
+		"192.0.0.0/24",
+		"192.0.2.0/24",
+		"192.168.0.0/16",
+		"198.18.0.0/15",
+		"198.51.100.0/24",
+		"203.0.113.0/24",
+		"240.0.0.0/4",
+		"100::/64",
+		"2001:2::/48",
+		"2001:db8::/32",
+		"fc00::/7",
+		"fe80::/10",
 	}
 
 	var addrs []*net.IPNet
@@ -108,7 +108,7 @@ func NewHead(ctx context.Context, options ...opts.Option) (*Head, chan Bootstrap
 
 	addrFilters, err := getAddrFilters()
 	if err != nil {
-		panic("This is a programming error (for now).")
+		panic(err)
 	}
 
 	libp2pOpts := []libp2p.Option{
