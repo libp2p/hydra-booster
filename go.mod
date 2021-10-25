9da629f5ae7c6a1b17f09ab7367935cc6b04c132
Petar Maymounkov <petarm@gmail.com>
2021-10-25 08:47:48 -0700

    first draft end-to-end test

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ require (
 	github.com/alanshaw/ipfs-ds-postgres v0.1.1
 	github.com/alanshaw/ipfs-hookds v0.3.0
 	github.com/alanshaw/prom-metrics-client v0.3.0
-	github.com/aschmahmann/vole v0.0.0-20210817234253-152e7d892038
 	github.com/axiomhq/hyperloglog v0.0.0-20191112132149-a4c4c47bc57f
 	github.com/dustin/go-humanize v1.0.0
 	github.com/gopherjs/gopherjs v0.0.0-20190812055157-5d271430af9f // indirect
@@ -22,14 +21,13 @@ require (
 	github.com/libp2p/go-libp2p-circuit v0.4.0
 	github.com/libp2p/go-libp2p-connmgr v0.2.4
 	github.com/libp2p/go-libp2p-core v0.9.0
-	github.com/libp2p/go-libp2p-host v0.1.0
 	github.com/libp2p/go-libp2p-kad-dht v0.13.2-0.20211020232845-0e473c3a776b
 	github.com/libp2p/go-libp2p-kbucket v0.4.7
 	github.com/libp2p/go-libp2p-noise v0.2.2
 	github.com/libp2p/go-libp2p-peerstore v0.3.0
-	github.com/libp2p/go-libp2p-protocol v0.1.0
 	github.com/libp2p/go-libp2p-quic-transport v0.12.0
 	github.com/libp2p/go-libp2p-record v0.1.3
+	github.com/libp2p/go-libp2p-swarm v0.5.3
 	github.com/libp2p/go-libp2p-tls v0.2.0
 	github.com/libp2p/go-tcp-transport v0.2.8
 	github.com/multiformats/go-base32 v0.0.3
@@ -38,6 +36,7 @@ require (
 	github.com/prometheus/client_golang v1.11.0
 	github.com/prometheus/statsd_exporter v0.21.0 // indirect
 	github.com/smartystreets/assertions v1.0.1 // indirect
+	github.com/stretchr/testify v1.7.0
 	github.com/whyrusleeping/timecache v0.0.0-20160911033111-cfcb2f1abfee
 	go.opencensus.io v0.23.0
 	golang.org/x/crypto v0.0.0-20210813211128-0a44fdfbc16e
--- testing/delegated_test.go
+++ testing/delegated_test.go
@@ -6,14 +6,17 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	vole "github.com/aschmahmann/vole/lib"
 	"github.com/ipfs/go-cid"
 	"github.com/ipfs/go-delegated-routing/client"
 	"github.com/ipfs/go-delegated-routing/server"
+	ipfsutil "github.com/ipfs/go-ipfs-util"
 	"github.com/libp2p/go-libp2p-core/peer"
-	protocol "github.com/libp2p/go-libp2p-protocol"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	swarmt "github.com/libp2p/go-libp2p-swarm/testing"
+	bhost "github.com/libp2p/go-libp2p/p2p/host/basic"
 	"github.com/libp2p/hydra-booster/head/opts"
 	"github.com/multiformats/go-multiaddr"
+	"github.com/stretchr/testify/require"
 )
 
 func TestDelegatedRoutingEndToEnd(t *testing.T) {
@@ -23,7 +26,7 @@ func TestDelegatedRoutingEndToEnd(t *testing.T) {
 
 	// start hydra head
 	headTcpAddr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", 35121))
-	_, err := SpawnHead(
+	head, err := SpawnHead(
 		context.Background(),
 		opts.Addrs([]multiaddr.Multiaddr{headTcpAddr}),
 		opts.Delegate(s.URL),
@@ -31,9 +34,42 @@ func TestDelegatedRoutingEndToEnd(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	t.Logf("started hydra %v at %v", head.Host.ID(), headTcpAddr)
+
+	// create DHT client
+	dhtCtx := context.Background()
+	dhtOpts := []dht.Option{
+		// dht.NamespacedValidator("v", blankValidator{}),
+		dht.DisableAutoRefresh(),
+		dht.Mode(dht.ModeClient),
+	}
+	host, err := bhost.NewHost(dhtCtx, swarmt.GenSwarm(t, dhtCtx, swarmt.OptDisableReuseport), new(bhost.HostOpts))
+	require.NoError(t, err)
+
+	d, err := dht.New(dhtCtx, host, dhtOpts...)
+	require.NoError(t, err)
+	t.Logf("started dht %v", host.ID())
+
+	// add the hydra head to the DHT routing table
+	err = host.Connect(context.Background(), peer.AddrInfo{ID: head.Host.ID(), Addrs: head.Host.Addrs()})
+	if err != nil {
+		t.Fatalf("connecting dht to head (%v)", err)
+	}
+	ok, err := d.RoutingTable().TryAddPeer(head.Host.ID(), true, false)
+	if !ok || err != nil {
+		t.Fatalf("cannot add peer to table")
+	}
 
 	// query hydra head
-	vole.DhtGetProvs(context.Background(), []byte{0x00}, protocol.ID("/ipfs"), headTcpAddr)
+	key := cid.NewCidV1(cid.Raw, ipfsutil.Hash([]byte("testkey")))
+	infos, err := d.FindProviders(dhtCtx, key)
+	require.NoError(t, err)
+	if len(infos) != 1 {
+		t.Fatalf("expecting a single provider")
+	}
+	if !equalAddrInfos(infos[0], testAddrInfo) {
+		t.Errorf("expecting %v, got %v", testAddrInfo, infos[0])
+	}
 }
 
 // testFindProvidersAsyncFunc responds with the same provider for any query key.
@@ -45,10 +81,25 @@ func testFindProvidersAsyncFunc(key cid.Cid, ch chan<- client.FindProvidersAsync
 	return nil
 }
 
+func equalAddrInfos(x, y peer.AddrInfo) bool {
+	if x.ID != y.ID {
+		return false
+	}
+	if len(x.Addrs) != len(y.Addrs) {
+		return false
+	}
+	for i := range x.Addrs {
+		if !x.Addrs[i].Equal(y.Addrs[i]) {
+			return false
+		}
+	}
+	return true
+}
+
 var testAddrInfo peer.AddrInfo
 
 func init() {
-	ma := multiaddr.StringCast("/ip4/7.7.7.7/tcp/4242/p2p/QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N")
+	ma := multiaddr.StringCast("/ip4/127.0.0.1/tcp/14242/p2p/QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N")
 	ai, err := peer.AddrInfoFromP2pAddr(ma)
 	if err != nil {
 		panic(err)
