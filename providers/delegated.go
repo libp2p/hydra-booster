9512c1c8cdbf26014f114a82932938019eaf82af
Petar Maymounkov <petarm@gmail.com>
2021-10-26 09:05:27 -0700

    add basic metrics on delegated path

--- providers/delegated.go
+++ providers/delegated.go
@@ -10,7 +10,10 @@ import (
 	"github.com/ipfs/go-delegated-routing/client"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-kad-dht/providers"
+	"github.com/libp2p/hydra-booster/metrics"
 	"github.com/multiformats/go-multihash"
+	"go.opencensus.io/stats"
+	"go.opencensus.io/tag"
 )
 
 func DelegateProvider(endpointURL string, timeout time.Duration) (providers.ProviderStore, error) {
@@ -34,5 +37,24 @@ func (s *DelegatedProviderStore) GetProviders(ctx context.Context, key []byte) (
 	if err != nil {
 		return nil, err
 	}
-	return s.c.FindProviders(ctx, cid.NewCidV1(cid.Raw, h))
+	t0 := time.Now()
+	infos, err := s.c.FindProviders(ctx, cid.NewCidV1(cid.Raw, h))
+	dur := time.Now().Sub(t0)
+	recordFindProvsComplete(ctx, statusFromErr(err), metrics.FindProvsDuration.M(float64(dur)))
+	return infos, err
+}
+
+func recordFindProvsComplete(ctx context.Context, status string, extraMeasures ...stats.Measurement) {
+	stats.RecordWithTags(
+		ctx,
+		[]tag.Mutator{tag.Upsert(metrics.KeyStatus, status)},
+		append([]stats.Measurement{metrics.DelegatedFindProvs.M(1)}, extraMeasures...)...,
+	)
+}
+
+func statusFromErr(err error) string {
+	if err != nil {
+		return "failure"
+	}
+	return "success"
 }
