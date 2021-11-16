b351ab61e1a8533824497b883db5c98bc737e69b
thattommyhall <thattommyhall@gmail.com>
2021-11-16 01:15:57 +0100

    Make stuff under k8s an example

--- k8s/copy_image.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker pull docker.io/libp2p/hydra-booster
-docker tag docker.io/libp2p/hydra-booster registry.digitalocean.com/protocol/libp2p/hydra-booster
-docker push registry.digitalocean.com/protocol/libp2p/hydra-booster
--- k8s/deploy_DO.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -u
-set -e
-docker build -t libp2p/hydra-booster:latest ../
-docker tag libp2p/hydra-booster:latest registry.digitalocean.com/protocol/libp2p/hydra-booster:latest
-docker push registry.digitalocean.com/protocol/libp2p/hydra-booster:latest
